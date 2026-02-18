44bae4edaac423bcff9ec07e85d38234b714481a
thetechleaf90-creator <thetechleaf90@gmail.com>
2026-02-18 21:46:10 +0530

    Create login_sreen.dart

---
+++ login_sreen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Login Screen",
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Header and Icons
+                Icon(Icons.lock_person_rounded, size: 120, color: Colors.teal),
+                SizedBox(height: 8),
+                Text("Welcome Back", style: TextStyle(fontSize: 40)),
+                SizedBox(height: 12),
+                // Email Id
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 12),
+                // Password
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                // Forgor Password
+                Align(
+                  alignment: Alignment.topRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                // Login Button
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 80),
+                    backgroundColor: Colors.teal,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+                // Sing Up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Don't have an account",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Sign Up", style: TextStyle(fontSize: 18)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
